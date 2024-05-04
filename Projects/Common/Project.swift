f2283462f4e1f57e621b3e7c697857019d07074a
ryuhyeaonJin <dbguswls030@gmail.com>
2024-05-05 01:49:09 +0900

    [chore] Common target 추가(assets, font 등)
    [chore] Common target 추가(assets, font 등)

--- Projects/Common/Project.swift
+++ Projects/Common/Project.swift
@@ -5,4 +5,21 @@
 //  Created by 유현진 on 5/5/24.
 //
 
-import Foundation
+import ProjectDescription
+
+let project = Project(
+    name: "Common",
+    targets: [
+        .target(
+            name: "Common",
+            destinations: .iOS,
+            product: .framework,
+            bundleId: "com.RunningHi.common",
+            infoPlist: .default,
+            sources: ["Sources/**"],
+            resources: ["Resources/**"],
+            dependencies: [
+            ]
+        ),
+    ]
+)
