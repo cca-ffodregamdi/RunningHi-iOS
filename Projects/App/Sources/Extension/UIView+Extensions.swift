7099ab20577af10596cf17f0b111880e68316599
오영석 <oys9330@gmail.com>
2024-05-08 00:18:41 +0900

    Feat: [App[ UIView+Extensions 추가

---
+++ Projects/App/Sources/Extension/UIView+Extensions.swift
@@ -0,0 +1,63 @@
+//
+//  UIView+Extensions.swift
+//  App
+//
+//  Created by 오영석 on 5/7/24.
+//
+
+import UIKit
+
+public extension UIView {
+    /// 기본 배경색 설정
+    func configBackgroundColor(color: UIColor = .systemBackground) {
+        self.backgroundColor = color
+    }
+    
+    /// 배경 탭하면 키보드 내리기
+    func tappedDismissKeyboard() {
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        tapGesture.cancelsTouchesInView = false
+        self.addGestureRecognizer(tapGesture)
+    }
+    @objc private func dismissKeyboard() {
+        self.endEditing(true)
+    }
+    
+    /// [UIView] 배열을 view 에 추가하기
+    func addSubview(_ views: [UIView]) {
+        views.forEach {
+            self.addSubview($0)
+        }
+    }
+}
+
+// MARK: - 그림자 관련
+extension UIView {
+    enum VerticalLocation {
+        case bottom
+        case top
+        case left
+        case right
+    }
+
+    func addShadow(location: VerticalLocation, color: UIColor = .black, opacity: Float = 0.8, radius: CGFloat = 5.0) {
+        switch location {
+        case .bottom:
+             addShadow(offset: CGSize(width: 0, height: 10), color: color, opacity: opacity, radius: radius)
+        case .top:
+            addShadow(offset: CGSize(width: 0, height: -10), color: color, opacity: opacity, radius: radius)
+        case .left:
+            addShadow(offset: CGSize(width: -10, height: 0), color: color, opacity: opacity, radius: radius)
+        case .right:
+            addShadow(offset: CGSize(width: 10, height: 0), color: color, opacity: opacity, radius: radius)
+        }
+    }
+
+    func addShadow(offset: CGSize, color: UIColor = .gray, opacity: Float = 0.1, radius: CGFloat = 3.0) {
+        self.layer.masksToBounds = false
+        self.layer.shadowColor = color.cgColor
+        self.layer.shadowOffset = offset
+        self.layer.shadowOpacity = opacity
+        self.layer.shadowRadius = radius
+    }
+}
