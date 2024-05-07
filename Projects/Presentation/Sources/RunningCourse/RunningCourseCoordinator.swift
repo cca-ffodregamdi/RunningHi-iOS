fc10f8ac3914b05284011424d7c11602cecaddf2
오영석 <oys9330@gmail.com>
2024-05-08 00:25:55 +0900

    Feat: [Presentation] RunningCourseCoordinator, RunningCourseViewController 추가

---
+++ Projects/Presentation/Sources/RunningCourse/RunningCourseCoordinator.swift
@@ -0,0 +1,8 @@
+//
+//  RunningCourseCoordinator.swift
+//  Presentation
+//
+//  Created by 오영석 on 5/7/24.
+//
+
+import Foundation
---
+++ Projects/Presentation/Sources/RunningCourse/RunningCourseViewController.swift
@@ -0,0 +1,18 @@
+//
+//  RunningCourseViewController.swift
+//  Presentation
+//
+//  Created by 오영석 on 5/7/24.
+//
+
+import Foundation
+import MapKit
+
+class RunningCourseViewController: BaseViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
