27db31e0dc00d49537002572b7629ec00144bdbc
오영석 <oys9330@gmail.com>
2024-05-08 10:42:19 +0900

    Feat: RunningCourseCoordinator 추가

--- Projects/Presentation/Sources/RunningCourse/RunningCourseCoordinator.swift
+++ Projects/Presentation/Sources/RunningCourse/RunningCourseCoordinator.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+import UIKit
+
+protocol RunningCourseCoordinatorDelegate{
+    func didFinishCourse()
+}
+
+class RunningCourseCoordinator: Coordinator, RunningCourseCoordinatorDelegate, RunningCourseViewControllerDelegate{
+    
+    var delegate: LoginCoordinatorDelegate?
+    
+    var childCoordinator: [Coordinator] = []
+    
+    private var navigationController: UINavigationController!
+    
+    public init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+    
+    func start() {
+        let vc = RunningCourseViewController()
+        vc.delegate = self
+        self.navigationController.viewControllers = [vc]
+    }
+    
+    func didFinishCourse() {
+    }
+}
