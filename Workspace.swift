8f5b43434ec39efa4013ec5a8c5c0a634b41f4ca
ryuhyeaonJin <dbguswls030@gmail.com>
2024-05-14 18:15:09 +0900

    Chore: 누수 파일 추가

---
+++ Workspace.swift
@@ -0,0 +1,13 @@
+//
+//  Workspace.swift
+//  Packages
+//
+//  Created by 유현진 on 5/1/24.
+//
+
+import ProjectDescription
+
+let workspace = Workspace(
+    name: "Workspace",
+    projects: ["Projects/**"]
+)
