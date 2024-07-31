86f31feb91ea4993a4b6aca1d8979d1462c27e39
ryuhyeaonJin <dbguswls030@gmail.com>
2024-07-31 12:44:40 +0900

    feat: 북마크, 좋아요 버튼 이미지 추가 및 수정

--- Projects/Presentation/Sources/Feed/Feed/Views/Cell/FeedCollectionViewCell.swift
+++ Projects/Presentation/Sources/Feed/Feed/Views/Cell/FeedCollectionViewCell.swift
@@ -39,7 +39,7 @@ class FeedCollectionViewCell: UICollectionViewCell {
     lazy var bookmarkButton: UIButton = {
         let button = UIButton()
         button.setImage(CommonAsset.bookmarkOutline.image, for: .normal)
-        button.setImage(CommonAsset.bookmarkFilled.image, for: .selected)
+        button.setImage(CommonAsset.bookmarkBlue.image, for: .selected)
         button.backgroundColor = UIColor.colorWithRGB(r: 255, g: 255, b: 255, alpha: 0.6)
         button.clipsToBounds = false
         return button
