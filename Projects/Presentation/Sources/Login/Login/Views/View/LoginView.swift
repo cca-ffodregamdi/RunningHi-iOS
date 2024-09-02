8a517e3e3d17cd1fe7d699782fec1c9915432c4b
ryuhyeaonJin <dbguswls030@gmail.com>
2024-09-02 18:37:59 +0900

    feat: 로그인 화면 백그라운드, 로고 이미지 추가

--- Projects/Presentation/Sources/Login/Login/Views/View/LoginView.swift
+++ Projects/Presentation/Sources/Login/Login/Views/View/LoginView.swift
@@ -11,11 +11,26 @@ import SnapKit
 
 class LoginView: UIView {
     
+    private lazy var backgroundImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = CommonAsset.onboardingImage.image
+        imageView.alpha = 0.5
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    
+    private lazy var logoImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = CommonAsset.logoSymbol.image
+        return imageView
+    }()
+    
     private lazy var welcomeTextLabel: UILabel = {
         let label = UILabel()
-        label.text = "러닝하이와 함께 뛰어볼까요?"
+        label.text = "러닝하이와 함께\n달릴 준비 되셨나요?"
         label.numberOfLines = 0
-        label.textAlignment = .center
+        label.font = .Headline
+        label.textColor = .Primary
         return label
     }()
     
@@ -33,7 +48,7 @@ class LoginView: UIView {
         button.configureBackgroundColor(color: .colorWithRGB(r: 254, g: 229, b: 0))
         button.configureImageView(image: CommonAsset.kakaoLogo.image)
         button.setTitleLabelText(text: "카카오로 시작하기")
-        button.configureTitleLabel(textColor: .colorWithRGB(r: 0, g: 0, b: 0, alpha: 0.85), font: UIFont.systemFont(ofSize: 13, weight: .regular))
+        button.configureTitleLabel(textColor: .colorWithRGB(r: 0, g: 0, b: 0, alpha: 0.85), font: UIFont.systemFont(ofSize: 14, weight: .regular))
         return button
     }()
     
@@ -42,7 +57,7 @@ class LoginView: UIView {
         button.configureBackgroundColor(color: .black)
         button.configureImageView(image: CommonAsset.appleLogo.image)
         button.setTitleLabelText(text: "Apple로 시작하기")
-        button.configureTitleLabel(textColor: .white, font: UIFont.systemFont(ofSize: 13, weight: .regular))
+        button.configureTitleLabel(textColor: .white, font: UIFont.systemFont(ofSize: 14, weight: .regular))
         return button
     }()
     
@@ -57,18 +72,29 @@ class LoginView: UIView {
     
     func configureUI(){
         self.backgroundColor = .colorWithRGB(r: 34, g: 101, b: 201, alpha: 0.3)
-        
+        self.addSubview(backgroundImageView)
+        self.addSubview(logoImageView)
         self.addSubview(welcomeTextLabel)
         self.addSubview(loginButtonStackView)
         
-        [kakaoLoginButton, appleLoginButton].forEach{
-            self.loginButtonStackView.addArrangedSubview($0)
+        backgroundImageView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+        
+        logoImageView.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(200)
+            make.left.equalToSuperview().inset(40)
+            make.width.height.equalTo(100)
         }
         
         welcomeTextLabel.snp.makeConstraints { make in
-            make.top.equalToSuperview().dividedBy(3.2)
-            make.left.equalToSuperview().offset(40)
-            make.right.equalToSuperview().offset(-40)
+            make.top.equalTo(logoImageView.snp.bottom).offset(-10)
+            make.left.equalToSuperview().inset(40)
+            
+        }
+        
+        [kakaoLoginButton, appleLoginButton].forEach{
+            self.loginButtonStackView.addArrangedSubview($0)
         }
         
         loginButtonStackView.snp.makeConstraints { make in
