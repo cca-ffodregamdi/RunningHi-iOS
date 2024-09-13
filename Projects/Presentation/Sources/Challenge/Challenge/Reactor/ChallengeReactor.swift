47073cd23876cba8473189c10587e9b563a4d39f
ryuhyeaonJin <dbguswls030@gmail.com>
2024-09-13 21:46:21 +0900

    feat: 챌린지 section 이름 변경

--- Projects/Presentation/Sources/Challenge/Challenge/Reactor/ChallengeReactor.swift
+++ Projects/Presentation/Sources/Challenge/Challenge/Reactor/ChallengeReactor.swift
@@ -49,7 +49,7 @@ public class ChallengeReactor: Reactor{
         case .setChallengeSection(let notParticipating, let participating, let completed):
             let notParticipatingSection = ChallengeSectionModel(header: "\(Date.getCurrentMonth())월의 챌린지", items: notParticipating.map{ChallengeItem.notParticipating($0)})
             let participatingSection = ChallengeSectionModel(header: "참여 중인 챌린지", items: participating.map{ChallengeItem.participating($0)})
-            let completedSection = ChallengeSectionModel(header: "완료한 챌린지", items: completed.map{ChallengeItem.completed($0)})
+            let completedSection = ChallengeSectionModel(header: "지난 챌린지", items: completed.map{ChallengeItem.completed($0)})
             newState.section = [participatingSection, notParticipatingSection, completedSection]
         }
         return newState
