076fbe31c4458882e390257acd46c70bbc15e7bf
dbguswls030 <dbguswls030@gmail.com>
2025-05-27 18:59:08 +0900

    chore: 서드파티 버전 갱신

--- Tuist/Package.swift
+++ Tuist/Package.swift
@@ -32,13 +32,13 @@ import PackageDescription
 let package = Package(
     name: "RunningHi",
     dependencies: [
-        .package(url: "https://github.com/Moya/Moya.git", .upToNextMajor(from: "15.0.0")),
+        .package(url: "https://github.com/Moya/Moya.git", .upToNextMajor(from: "15.0.3")),
         .package(url: "https://github.com/SnapKit/SnapKit.git", .upToNextMajor(from: "5.7.0")),
-        .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "6.7.0")),
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "6.9.0")),
         .package(url: "https://github.com/ReactorKit/ReactorKit.git", .upToNextMajor(from: "3.2.0")),
         .package(url: "https://github.com/onevcat/Kingfisher.git", .upToNextMajor(from: "7.0.0")),
-        .package(url: "https://github.com/kakao/kakao-ios-sdk-rx.git", .upToNextMajor(from: "2.22.1")),
-        .package(url: "https://github.com/RxSwiftCommunity/RxDataSources.git", .upToNextMajor(from: "5.0.0")),
+        .package(url: "https://github.com/kakao/kakao-ios-sdk-rx.git", .upToNextMajor(from: "2.24.2")),
+        .package(url: "https://github.com/RxSwiftCommunity/RxDataSources.git", .upToNextMajor(from: "5.0.2")),
         .package(url: "https://github.com/ChartsOrg/Charts.git", .upToNextMajor(from: "5.1.0"))
     ]
 )
